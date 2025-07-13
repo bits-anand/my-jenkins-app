701ec89dc6edcef57377f119efcd54686a51e1d9
bits-anand <2021wa86854@wilp.bits-pilani.ac.in>
2025-07-13 16:51:27 +0530

    Modifying source code 2

--- src/main/java/com/example/ci/App.java
+++ src/main/java/com/example/ci/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello from Jenkins CI - Code Change Detected!" );
+        System.out.println( "Hello from Jenkins CI - Code Change Detected!!" );
     }
 }
